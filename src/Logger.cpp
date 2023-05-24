9e86eb2cc77f0797e7f98f27184ecd6eaa9bf078
p3dr4m <p3dr4m@hotmail.com>
2023-05-24 15:19:32 -0700

    update

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,5 +1,73 @@
-//
-// Created by Raziq on 2023-04-26.
-//
-
+#include <iostream>
+#include <sstream>
+#include <limits>
+#include <ctime>
+#include <fstream>
 #include "Logger.h"
+
+void Logger::log(std::string message) {
+    writeToLog(message, logFileName);
+}
+
+void Logger::log(std::string message, LogType type, bool sepBegin, bool sepEnd) {
+    std::string log;
+
+    if (sepBegin)
+        log += separator;
+
+    switch (type) {
+        case INFO:
+            log += "INFO: " + message;
+            break;
+        case WARNING:
+            log += "WARNING: " + message;
+            break;
+        case ERROR:
+            log += "ERROR: " + message;
+            break;
+        case WORLD:
+            log += worldLog + message;
+            break;
+        case END:
+            cmdNum++;
+            log += std::to_string(cmdNum) + ": " + message + separator;
+            break;
+        case IMPORT:
+            cmdNum++;
+            log += "Command " + std::to_string(cmdNum) + ": " + message;
+            break;
+        case DEBUG:
+            cmdNum++;
+            log += "Command " + std::to_string(cmdNum) + ": " + message;
+            break;
+        case WHAT_IS:
+            cmdNum++;
+            log += "Command " + std::to_string(cmdNum) + ": " + message;
+            break;
+        case WHAT_IS_AT:
+            cmdNum++;
+            log += "Command " + std::to_string(cmdNum) + ": " + message;
+            break;
+        case WHAT_IS_IN:
+            cmdNum++;
+            log += "Command " + std::to_string(cmdNum) + ": " + message;
+            break;
+    }
+
+    if (sepEnd)
+        log += separator;
+
+    // write to log
+    writeToLog(log, logFileName);
+
+}
+
+//Export one line to a file.
+void Logger::writeToLog(std::string line, std::string filename) {
+    // output file
+    std::ofstream out;
+    out.open(filename, std::ios_base::app);
+
+    out << line << "\n";
+    out.close();
+}
