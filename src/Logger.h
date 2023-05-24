e25efdfa1f632826e5095387333a6f10d7c72b8b
p3dr4m <p3dr4m@hotmail.com>
2023-05-24 15:19:37 -0700

    Changes by Raziq

--- src/Logger.h
+++ src/Logger.h
@@ -5,8 +5,35 @@
 #ifndef FINALPROJALGO_LOGGER_H
 #define FINALPROJALGO_LOGGER_H
 
+#pragma once
+#include <string>
 
 class Logger {
+  public:
+    enum LogType {
+      INFO,
+      WARNING,
+      ERROR,
+      END,
+      WORLD,
+      IMPORT,
+      DEBUG,
+      WHAT_IS,
+      WHAT_IS_AT,
+      WHAT_IS_IN
+    };
+
+    std::string logFileName;
+    void log(std::string msg);
+    void log(std::string message, LogType type, bool sepBegin, bool sepEnd);
+    void writeToLog(std::string line, std::string filename);
+
+    std::string separator = "\n------------------------------------------------------------------------------------------\n";
+  private:
+    int cmdNum = 0;
+    std::string worldLog = "Latitude / longitude values in index entries are shown as floating point values, in decimal long/lat format."+ separator + "\t\t\t\t\t\tWorld boundaries are set to:\n";
+    std::string initialLog = "Course Project for COMP 8042\nStudent Name : Kevin Vilanova, Student Id : A01019400\nBegin of GIS Program log : \n";
+
 
 };
 
