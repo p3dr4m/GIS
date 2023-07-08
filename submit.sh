4d7fdb21a81e7ed6ee2ca9727ee7563d528c78ea
p3dr4m <p3dr4m@hotmail.com>
2023-07-08 14:32:54 -0700

    update

--- submit.sh
+++ submit.sh
@@ -7,3 +7,14 @@ cp Makefile ./submit
 cp class_diagram.png ./submit
 cp ./doc/GIS_System_Algo_Report.pdf ./submit
 
+# Navigate into the 'submit' directory
+cd ./submit || exit
+
+# Create the zip from within the 'submit' directory
+zip -r ../submit.zip ./*
+
+# Navigate back out of the 'submit' directory
+cd ..
+
+# Remove the 'submit' directory
+rm -rf ./submit
